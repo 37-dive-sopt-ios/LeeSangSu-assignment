b17617efa7d449064303497c9d2be2b4f68f8e5b
soo127 <ss_127@naver.com>
2025-10-31 14:45:39 +0900

    Feat: 로그인창 UI

---
+++ LeeSangSu-assignment/ViewControllers/LoginViewController.swift
@@ -0,0 +1,126 @@
+//
+//  LoginViewController.swift
+//  LeeSangSu-assignment
+//
+//  Created by 이상수 on 10/31/25.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    let emailField = UITextField()
+    let passwordField = UITextField()
+    let loginButton = UIButton()
+    let findAccountButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        setupSubviews()
+        setupLayout()
+    }
+    
+}
+
+extension LoginViewController {
+    
+    private func setupSubviews() {
+        setupEmailField()
+        setupPasswordField()
+        setupLoginButton()
+        setupFindAccountButton()
+    }
+    
+    private func setupEmailField() {
+        view.addSubview(emailField)
+        emailField.placeholder = "이메일 아이디"
+        emailField.borderStyle = .none
+        emailField.layer.cornerRadius = 5
+        emailField.layer.borderWidth = 1
+        emailField.layer.borderColor = UIColor.gray.cgColor
+        emailField.addLeftPadding()
+        emailField.addRightPadding()
+    }
+    
+    private func setupPasswordField() {
+        view.addSubview(passwordField)
+        passwordField.placeholder = "비밀번호"
+        passwordField.borderStyle = .none
+        passwordField.layer.cornerRadius = 5
+        passwordField.layer.borderWidth = 1
+        passwordField.layer.borderColor = UIColor.gray.cgColor
+        passwordField.addLeftPadding()
+        passwordField.addRightPadding()
+    }
+    
+    private func setupLoginButton() {
+        view.addSubview(loginButton)
+        loginButton.backgroundColor = .lightGray
+        loginButton.setTitle("로그인", for: .normal)
+        loginButton.layer.cornerRadius = 5
+    }
+    
+    private func setupFindAccountButton() {
+        view.addSubview(findAccountButton)
+        findAccountButton.setTitle("계정 찾기 >", for: .normal)
+        findAccountButton.setTitleColor(.black, for: .normal)
+    }
+    
+}
+
+extension LoginViewController {
+    
+    private func setupLayout() {
+        layoutEmailField()
+        layoutPasswordField()
+        layoutLoginButton()
+        layoutFindAccountButton()
+    }
+    
+    private func layoutEmailField() {
+        emailField.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 100, left: 20, bottom: 0, right: 20),
+            height: 40
+        )
+    }
+    
+    private func layoutPasswordField() {
+        passwordField.anchor(
+            top: emailField.bottomAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 10, left: 20, bottom: 0, right: 20),
+            height: 40
+        )
+    }
+    
+    private func layoutLoginButton() {
+        loginButton.anchor(
+            top: passwordField.bottomAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 20, left: 20, bottom: 0, right: 20),
+            height: 40
+        )
+    }
+    
+    private func layoutFindAccountButton() {
+        findAccountButton.anchor(
+            top: loginButton.bottomAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 23, left: 20, bottom: 0, right: 20),
+            height: 40
+        )
+    }
+    
+}
+
+#Preview { LoginViewController() }
