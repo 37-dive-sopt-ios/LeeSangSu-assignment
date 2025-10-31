37437626af69b8f396d29c6934cb001c40779140
soo127 <ss_127@naver.com>
2025-10-31 12:29:08 +0900

    init: 프로젝트 기본 구조

---
+++ LeeSangSu-assignmentTests/LeeSangSu_assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeeSangSu_assignmentTests.swift
+//  LeeSangSu-assignmentTests
+//
+//  Created by 이상수 on 10/31/25.
+//
+
+import Testing
+@testable import LeeSangSu_assignment
+
+struct LeeSangSu_assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
