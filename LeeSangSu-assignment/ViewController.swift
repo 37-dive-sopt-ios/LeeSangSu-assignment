421558f628a21d5b45132024fc1594f2bb6d464a
soo127 <ss_127@naver.com>
2025-10-31 12:37:11 +0900

    chore: code base

--- LeeSangSu-assignment/ViewController.swift
+++ LeeSangSu-assignment/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
-
+    
+    private let testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 15)
+        label.clipsToBounds = true
+        return label
+    }()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
+        setLayout()
     }
-
-
+    
+    private func setUI() {
+        view.addSubview(testLabel)
+        view.backgroundColor = .yellow
+    }
+    
+    private func setLayout() {
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -150)
+        ])
+    }
 }
 
