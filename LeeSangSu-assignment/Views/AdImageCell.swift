0273144fb10fec0d2a7f815c13d44c693a6c892a
soo127 <ss_127@naver.com>
2025-11-11 01:53:38 +0900

    Feat: 개별 광고 이미지 셀

---
+++ LeeSangSu-assignment/Views/AdImageCell.swift
@@ -0,0 +1,28 @@
+//
+//  AdImageCell.swift
+//  LeeSangSu-assignment
+//
+//  Created by 이상수 on 11/11/25.
+//
+
+import UIKit
+import SnapKit
+
+final class AdImageCell: UICollectionViewCell {
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        imageView.contentMode = .scaleAspectFill
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+}
